e81727c65db19afe4ee296da98aefb05b6854556
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-15 21:04:43 +0300

    Changes to portfolio

--- js/data/portfolio-gallery/portfolioAllData.js
+++ js/data/portfolio-gallery/portfolioAllData.js
@@ -1,49 +1,49 @@
 const portfolioAllData = [
     {
         status: 'published',
-        imgName: '1.jpeg',
+        imgName: '1.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design1'
     },
     {
         status: 'published',
-        imgName: '2.jpeg',
+        imgName: '2.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design2'
     },
     {
         status: 'published',
-        imgName: '3.jpeg',
+        imgName: '3.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design3'
     },
     {
         status: 'published',
-        imgName: '4.jpeg',
+        imgName: '4.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design4'
     },
     {
         status: 'published',
-        imgName: '6.jpeg',
+        imgName: '6.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design6'
     },
     {
         status: 'published',
-        imgName: '5.jpeg',
+        imgName: '5.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design5'
     },
     {
         status: 'published',
-        imgName: '7.jpeg',
+        imgName: '7.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design7'
     },
     {
         status: 'published',
-        imgName: '8.jpeg',
+        imgName: '8.jpg',
         imgAltName: 'WEB Design',
         overlayTitle: 'WEB Design8'
     }
