194f12b5547e0927db594724089968e3fc657855
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-10-18 15:36:34 +0300

    Created title style, edited footer style and dinamic data script

--- js/components/slide-bar/slidebar.js
+++ js/components/slide-bar/slidebar.js
@@ -10,23 +10,18 @@ function renderHtml(i) {
                 <div class="percentBox">${el.value}%</div>
             </div>`
     });
-    console.log('HTMLSAS ', html);
     return html;
 }
 
 const html1 = renderHtml(0);
 const html2 = renderHtml(1);
 
-console.log(slidebarData);
-
 document.getElementById('slideBarContent').innerHTML =  html1;
 
 function slidebar() {
-    console.log('ISKIVETE ', document.getElementById('slide-bar'));
     const toggleBtn = document.getElementById('slide-bar');
     toggleBtn.addEventListener('click', () => {
         toggleBtn.classList.toggle('active')
-        console.log('CLASES ');
         document.getElementById('slideBarContent').innerHTML = toggleBtn.classList.value === 'active' ? html2 : html1;
     })
     
